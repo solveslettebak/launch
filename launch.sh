c7fbb9b5ccc6fae8e9efd4eb6c45ee826059c19f
Benjamin Bolling <benjamin.bolling@ess.eu>
2023-04-02 11:50:04 +0000

    Update launch.sh, SLconsole_menus.json

--- launch.sh
+++ launch.sh
@@ -3,9 +3,15 @@ die () {
   exit 1
 }
 
-[ "$#" -eq 1 ] || die "1 argument required, $# provided"
+[[ "$#" -eq 1 ] || [ "$#" -eq 2 ] ] || die "1 or 2 arguments required, $# provided"
 test -f "/nfs/Linacshare_controlroom/MCR/phoebus_configs/${1}/memento" || die "${1}: file not found"
 
 rm /home/operator-mcr/.phoebus/memento
 cp /nfs/Linacshare_controlroom/MCR/phoebus_configs/${1}/memento /home/operator-mcr/.phoebus/memento
-phoebus
+
+if TEST-COMMAND
+then
+  phoebus
+else
+  phoebus -settings ${2}
+fi
