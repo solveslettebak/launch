409eefe6e706c40a68e2696f45a6b7c5a0c0d926
Sölve Slettebak <solve.slettebak@ess.eu>
2023-06-04 18:53:21 +0200

    'Push layouts to git'-function now complete

--- gitpusher.py
+++ gitpusher.py
@@ -2,11 +2,16 @@ import os
 import sys
 
 path_ph = '/home/operator-mcr/.phoebus/'
-path_git = '/nfs/Linacshare_controlroom/MCR/launcher/' # shared-cs-studio-phoebus-layouts/
+path_git = '/nfs/Linacshare_controlroom/MCR/launcher/shared-cs-studio-phoebus-layouts/'
 
 found = False
 
 print('\n')
+
+print('This will copy files layout_* and window_* to',path_git,'and execute git add, commit and push. You need to supply your credentials.')
+i = input('Proceed? (y/n)')
+if not i.lower() == 'y':
+  sys.exit(0)
 
 for f in os.listdir(path_ph):
   if f.startswith('layout_') or f.startswith('window_'):
@@ -16,7 +21,7 @@ for f in os.listdir(path_ph):
     os.system(command)
 
 if not found:
-  print('No new layouts found')
+  input('No new layouts found. Press enter to exit')
   sys.exit(0)
 
 print('\n')
@@ -24,6 +29,9 @@ print('\n')
 os.chdir(path_git)
 assert os.getcwd() == path_git[:-1]
 message = input('git commit message (or ctrl+c to abort): ')
-os.system('git add .')
-os.system('git commit -m "'+message+'"')
-os.system('git push')
+if False:
+  os.system('git add .')
+  os.system('git commit -m "'+message+'"')
+  os.system('git push')
+
+input('Verify above output messages, and hit enter to exit')
